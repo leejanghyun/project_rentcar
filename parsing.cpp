7dec2c955e3a385f71251ff18ed63ca80999d0e4
leejanghyun <dlwkdgjs012@naver.com>
2016-06-07 11:56:37 +0900

    parsing.cpp
    parsing

--- parsing.cpp
+++ parsing.cpp
@@ -1,66 +1,20 @@
 
-int Parsing_Image(char* header){///filename+image
-	char* Last_HTML_File=NULL;////
-	strcpy(method, strtok(header," /"));////전체 패킷문입니다.
-	
-	while(1){/////////////////ex) .html/.html/...있을때 마지막 .html뽑기위한과정 입니다.///Image_FILE에는 이미지들이 담긴다.
-	char * Image_File=strtok(NULL," /");
-	    if(strstr(Image_File,".html")==NULL){
-			strcpy(fileName,Last_HTML_File);
-			strcpy(Image,Image_File);
-			break;
-	    }
-		Last_HTML_File=Image_File;
-	}
-
-	strcpy(ct,ContentType(fileName));///Content-Type 확인
-
-	return 1;
-}
-
-void Parsing_for_login(SOCKET Clntsock,char* ct,char* message,char* IP){
-	char identify[MIN_SIZE];
-	char id[MIN_SIZE];
-	char pw[MIN_SIZE];
-	char login[200]="";//////id pw 일치하면 데이터베이스 login=1로변경하기 위함
-	char IP_Field[200]="";///IP필드 저장위한 쿼리
-
-	int len=0;
+void Parsing_for_chat(SOCKET Clntsock,char* ct,char* Data,char* IP,char* id){
 	MYSQL* conn_ptr=NULL; // mysql 과의 연결 담당
     MYSQL_RES* res=NULL; // 쿼리 결과 받는 변수
     MYSQL_ROW row; //쿼리에 대한 실제 데이터 값이 들어있는 변수
-	
-	strcpy(identify,strstr(message,"login_id"));
-	strtok(identify,"=");
-	strcpy(id,strtok(NULL,"&"));////(id)&pw=(pw)
-	strtok(NULL,"=");//(id)저장
-	strcpy(pw,strtok(NULL,"="));//(pw)저장
+	char Query[MAX_SIZE]="INSERT INTO `rentcar`.`question` (`ID` ,`Q`) VALUES ('";
+	char Question[MIN_SIZE]="";
+	char message[MIN_SIZE]="";
+	strcpy(message,strstr(Data,"chat"));
+	strcpy(Question,message+5);
 
 	initMYSQL(&conn_ptr);
-	mysql_query(conn_ptr, Query1);
-	res = mysql_store_result(conn_ptr); 
+	strcat(Query,id);
+	strcat(Query,"','");
+	strcat(Query,Question);
+	strcat(Query,"' )");
+	mysql_query(conn_ptr, Query);//////////////////DV에 send로 보낸 메시지 ID와 Question 저장
 
-	while ((row=mysql_fetch_row(res))!=NULL)
-		{
-			if(!strcmp(id,row[1]) && !strcmp(pw,row[2])){      /////////////id pw 가 일치하는지 검산 row[1]:id row[2]:pw
-				strcat(login,Query2);
-				strcat(IP_Field,Query3);
-				strcat(login,"1' where ID='"); /////login 대면 1으로 변경
-				strcat(IP_Field,IP);
-				strcat(IP_Field,"' where ID='"); /////login 대면 1으로 변경
-				strcat(login,id);
-				strcat(IP_Field,id);
-				strcat(login,"'");
-				strcat(IP_Field,"'");
-
-	            len = mysql_query(conn_ptr, login);
-				len = mysql_query(conn_ptr, IP_Field);
-				main_html(Clntsock,ct,id);
-				return;                    //=>이유:로그인 한사람만 다음페이지로
-			}
-		}	
-	char err_message[MAX_SIZE]="<html><head><script type=\"text/javascript\">alert('login error')</script></head></html>";
-	send(Clntsock,err_message,strlen(err_message),0);
-	SendData(Clntsock,ct,"main.html");	//////메인페이지로 이동
-
+	question_html(Clntsock,ct,id,1);
 }
